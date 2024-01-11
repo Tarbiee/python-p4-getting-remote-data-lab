1ecdb2f1ccc7513a808ef2d489f0ca033bc64e3a
Tarbiee <dorcusstaby@gmail.com>
2024-01-11 14:53:22 +0300

    completed assignment

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,13 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
+        
 
     def load_json(self):
-        pass
+        requests = json.loads(self.get_response_body())
+        
+    
+get_requester = GetRequester('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
+print(get_requester.load_json())
